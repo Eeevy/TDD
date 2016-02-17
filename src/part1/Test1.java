67eb9326e45c97f99f8937d142624bb22f1add18
emmashakesp <ema@wikblad.com>
2016-02-17 11:54:47 +0100

    testfall

--- src/part1/Test1.java
+++ src/part1/Test1.java
@@ -1,54 +1,63 @@
 package part1;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 public class Test1 {
 	
-	public void testUppercaseLetters() {
-		// Använd assert.
-	}
-	
-	public void testLowercaseLetters() {
-		// Använd assert.
-	}
 	
 	@Test
-	public void test() {
+	public void testLowercaseLetters() {
 		rovar testClass = new rovar();
-		rovar.enrov("abcdefghijklmnopqrstuvxyzåäö");
-		rovar.enrov("åäö");
-		rovar.enrov("ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ");
-		rovar.enrov("00001237");
-		rovar.enrov("5");
-		rovar.enrov("");
-		rovar.enrov(null);
-		rovar.enrov("a77e666heJsaN");
-		rovar.enrov("007HAHA");
-		rovar.enrov("estar \n"
-				+ "att skriva fler rader \n"
-				+ "här");
-		rovar.enrov("en rad");
-		rovar.enrov("g");
-		rovar.enrov("giltigt input");
-		rovar.enrov("!”#°€%&/()=?`^^_.,<>");
 
-		rovar.derov("abcdefghijklmnopqrstuvxyzåäö");
-		rovar.derov("åäö");
-		rovar.derov("ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ");
-		rovar.derov("00001237");
-		rovar.derov("5");
-		rovar.derov("");
-		rovar.derov(null);
-		rovar.derov("a77e666heJsaN");
-		rovar.derov("007HAHA");
-		rovar.derov("estar \n"
-				+ "att skriva fler rader \n"
-				+ "här");
-		rovar.derov("en rad");
-		rovar.derov("g");
-		rovar.derov("giltigt input");
-		rovar.derov("!”#°€%&/()=?`^^_.,<>");
+		String input = "abcdefghijklmnopqrstuvxyzåäö";
+		String expected = "abobcocdodefofgoghohijojkoklolmomnonopopqoqrorsostotuvovxoxyzozåäö";
+		assertEquals(expected, rovar.enrov(input));
 	}
+	
+	public void testUppercaseLetters() {
+		String input = "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ";
+		String expected = "ABoBCoCDoDEFoFGoGHoHIJoJKoKLoLMoMNoNOPoPQoQRoRSoSToTUVoVXoXYZoZÅÄÖ";
+		assertEquals(expected, rovar.enrov(input));
+	}
+	
+//	@Test
+//	public void test() {
+//		rovar testClass = new rovar();
+//		rovar.enrov("abcdefghijklmnopqrstuvxyzåäö");
+//		rovar.enrov("åäö");
+//		rovar.enrov("ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ");
+//		rovar.enrov("00001237");
+//		rovar.enrov("5");
+//		rovar.enrov("");
+//		rovar.enrov(null);
+//		rovar.enrov("a77e666heJsaN");
+//		rovar.enrov("007HAHA");
+//		rovar.enrov("estar \n"
+//				+ "att skriva fler rader \n"
+//				+ "här");
+//		rovar.enrov("en rad");
+//		rovar.enrov("g");
+//		rovar.enrov("giltigt input");
+//		rovar.enrov("!”#°€%&/()=?`^^_.,<>");
+//
+//		rovar.derov("abcdefghijklmnopqrstuvxyzåäö");
+//		rovar.derov("åäö");
+//		rovar.derov("ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ");
+//		rovar.derov("00001237");
+//		rovar.derov("5");
+//		rovar.derov("");
+//		rovar.derov(null);
+//		rovar.derov("a77e666heJsaN");
+//		rovar.derov("007HAHA");
+//		rovar.derov("estar \n"
+//				+ "att skriva fler rader \n"
+//				+ "här");
+//		rovar.derov("en rad");
+//		rovar.derov("g");
+//		rovar.derov("giltigt input");
+//		rovar.derov("!”#°€%&/()=?`^^_.,<>");
+//	}
 }
