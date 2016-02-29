aea248bc865799bf80643bef3d5943a99a36aea9
Evelyn Gustavsson <Evy@evelyns-air.clients.internal.mah.se>
2016-02-29 15:30:19 +0100

    Några småändringar.

--- src/part1/Test2.java
+++ src/part1/Test2.java
@@ -9,77 +9,78 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Test2 {
+	private static Rovar testClass;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		rovar testClass = new rovar();
+		testClass = new Rovar();
 	}
 
 	@Test
 	public void testLowercaseLetters() {
 		String input = "abobcocdodefofgoghohijojkoklolmomnonopopqoqrorsostotuvovxoxyzozåäö";
 		String expected = "abcdefghijklmnopqrstuvxyzåäö";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 
 	@Test
 	public void testUppercaseLetters() {
 		String input = "ABOBCOCDODEFOFGOGHOHIJOJKOKLOLMOMNONOPOPQOQRORSOSTOTUVOVXOXYZOZÅÄÖ";
 		String expected = "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 
 	public void testStrangeLetters() {
 		String input = "zozåäö";
 		String expected = "zåäö";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 
 	public void testNumbers() {
 		String input = "00001237";
 		String expected = "00001237";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 
 	public void testNumber() {
 		String input = "5";
 		String expected = "5";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 
 	public void testEmptyString() {
 		String input = "";
 		String expected = "";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 
 	public void testNull() {
 		String input = null;
 		String expected = null;
-		assertNull(expected, rovar.derov(input));
+		assertNull(expected, Rovar.derov(input));
 	}
 
 	public void testMixed() {
 		String input = "a77e666hoheJoJsosaNoN";
 		String expected = "a77e666heJsaN";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 
 	public void testMixedAllChars() {
 		String input = "007HoHahohA*!";
 		String expected = "007HahA*!";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 	
 	public void testOneChar() {
 		String input = "gog";
 		String expected = "g";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 	
 	public void testSymbol() {
 		String input = "!”#°€%&/()=?`^^_.,<>";
 		String expected = "!”#°€%&/()=?`^^_.,<>";
-		assertEquals(expected, rovar.derov(input));
+		assertEquals(expected, Rovar.derov(input));
 	}
 }
