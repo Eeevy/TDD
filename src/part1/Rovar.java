b933efba463025b00b76a8ec585680b3e8d5fc6b
emmashakesp <ema@wikblad.com>
2016-02-29 16:03:13 +0100

    nu går vi hem

--- src/part1/Rovar.java
+++ src/part1/Rovar.java
@@ -4,8 +4,8 @@ package part1;
 public final class Rovar
 {
 
-    private static String lower_consonants = "bcdfhjklmnpqrstvwxz";//g saknas, därav fault!
-    private static String upper_consonants = "BCFGHJKLMNPQRSTVWXZ";//D saknas, därav fault!
+    private static String lower_consonants = "bcdfghjklmnpqrstvwxz";//g saknas, därav fault!
+    private static String upper_consonants = "BCDFGHJKLMNPQRSTVWXZ";//D saknas, därav fault!
 
     /// <summary>
     /// Encode the string in rovarspraket.
