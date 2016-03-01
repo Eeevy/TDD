40f3daf5200fd3b94060f0f04494d2b8902b2b34
emmashakesp <ema@wikblad.com>
2016-03-01 14:11:04 +0100

    init Clock

---
+++ src/part2/Clock.java
@@ -0,0 +1,19 @@
+package part2;
+
+public class Clock {
+	private Time theTime;
+	private Date theDate;
+	
+	public String changeMode(){
+		return null;
+	}
+	
+	public String reset(){
+		return null;
+	}
+	
+	public String set(int p2, int p3, int p4){
+		return null;
+	}
+
+}
